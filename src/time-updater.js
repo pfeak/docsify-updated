a4b43609488816114a05ef0df53e5fc00d75fdad
pfeak <pfeak163@163.com>
2022-04-10 00:46:54 +0800

    feat[code]: add new feature 'whereToPlace'
    'whereToPlace' use to set where show the label.

--- src/time-updater.js
+++ src/time-updater.js
@@ -1,14 +1,23 @@
 // Docsify plugin default values
-var defaultOptions = {
+let defaultOptions = {
   text: ">Last Modify: {docsify-updated}",
-  formatUpdated: '{YYYY}/{MM}/{DD}',
+  formatUpdated: "{YYYY}/{MM}/{DD}",
+  whereToPlace: "bottom"
 }
 
 // Docsify plugin functions
 function plugin(hook, vm) {
-  text = vm.config.timeUpdater.text
+  let text = vm.config.timeUpdater.text
+  let whereToPlace = String(vm.config.timeUpdater.whereToPlace).toLowerCase()
   hook.beforeEach(function (content) {
-    return content + '\n\n' + text
+    switch (whereToPlace) {
+      case "top":
+        return text + "\n\n" + content
+      case "bottom":
+        return content + "\n\n" + text
+      default:
+        return content + "\n\n" + text
+    }
   })
 }
 
@@ -25,5 +34,6 @@ window.$docsify.plugins = (window.$docsify.plugins || []).concat(plugin)
 //   timeUpdater: {
 //     text: ">last update time: {docsify-updated}",
 //     formatUpdated: '{YYYY}/{MM}/{DD}',
+//     whereToPlace: "bottom" // top || bottom
 //   }
 // }
